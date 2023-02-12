7cffa93edabf07f0ae457e92ac975e59c50477c9
Thái Thành Phát <skphat789@gmail.com>
2023-02-12 16:03:03 +0700

    Setup Axios

---
+++ src/api/axiosClient.js
@@ -0,0 +1,26 @@
+// api/axiosClient.js 
+import axios from 'axios'; 
+import queryString from 'query-string'; 
+// Set up default config for http requests here 
+// Please have a look at here `https://github.com/axios/axios#request config` for the full list of configs 
+const axiosClient = axios.create({ 
+ baseURL: process.env.REACT_APP_API_URL, 
+ headers: { 
+ 'content-type': 'application/json', 
+ }, 
+ paramsSerializer: 
+ {
+    serialize : (params) => queryString.stringify(params),
+    indexes: false
+}, 
+}); 
+axiosClient.interceptors.request.use(async (config) => {  // Handle token here ...  
+ return config; 
+}) 
+axiosClient.interceptors.response.use((response) => { 
+ return (response && response.data )|| response; 
+}, (error) => { 
+ // Handle errors 
+ throw error; 
+}); 
+export default axiosClient;
