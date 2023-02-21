fad0b03353a9838dce8ef1d8fa653dacf003ce6e
Thái Thành Phát <skphat789@gmail.com>
2023-02-21 13:31:00 +0700

    Setup Login with Redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { unwrapResult } from '@reduxjs/toolkit';
 // firebase
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
@@ -11,20 +13,20 @@ import ThemeProvider from './theme';
 // components
 import { StyledChart } from './components/chart';
 import ScrollToTop from './components/scroll-to-top';
-import loginApi from './api/loginApi';
+import { getMe } from './redux/Slice/userSlice';
+import { userLoginPublic } from './sections/auth/login/authSlice';
 
 // ----------------------------------------------------------------------
 export default function App() {
   const navigate = useNavigate();
   const [tokenId, setTokenId] = useState();
+  const dispatch = useDispatch();
 
   const fetchLogin = async () => {
     try {
-      console.log('tokenId', tokenId);
-      const response = await loginApi.getLoginPublic(tokenId);
-      localStorage.setItem('access-token', response.data.accessToken);
-      localStorage.setItem('refesh-token', response.data.refreshToken);
-      navigate('/dashboard', { replace: true });
+      const response = unwrapResult(await dispatch(userLoginPublic(tokenId)));;
+      const actionResult = dispatch(getMe());
+      const currentUser = unwrapResult(actionResult);
     } catch (error) {
       console.log('Fail to fetch Api: ', error.response);
     }
@@ -32,21 +34,29 @@ export default function App() {
   // Handle firebase auth changed
   useEffect(() => {
     const unregisterAuthObserver = firebase.auth().onAuthStateChanged(async (user) => {
-      if (!user && !localStorage.getItem('access-token')) {
-        navigate('/login', { replace: true });
+      if (!user) {
         return;
       }
       const tokenId = await user.getIdToken();
-      setTokenId(tokenId);
+      if(tokenId){
+        setTokenId(tokenId);
+      }
     });
     return () => unregisterAuthObserver();
-  }, []);
+  }, [navigate]);
 
   useEffect(() => {
     if (tokenId) {
       fetchLogin();
     }
   }, [tokenId]);
+
+  useEffect(() => {
+    if (localStorage.getItem('access-token') && tokenId === undefined) {
+      const actionResult = dispatch(getMe());
+      const currentUser = unwrapResult(actionResult);
+    }
+  }, [localStorage.getItem('access-token')]);
   // Handle get message push notify
   useEffect(() => {
     if (localStorage.getItem('access-token')) {
