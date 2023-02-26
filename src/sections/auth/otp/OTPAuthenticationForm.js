3addc635e22d1d3eba65305f35dfb69f5ca15c3e
vungmai-fptu <vungocmai322@gmail.com>
2023-02-26 21:10:07 +0700

    Done register/otp authentication flow

--- src/sections/auth/otp/OTPAuthenticationForm.js
+++ src/sections/auth/otp/OTPAuthenticationForm.js
@@ -1,29 +1,53 @@
 import { useState, useRef } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+// @mui
 import { Stack, TextField, Button } from '@mui/material';
 
-export default function OTP() {
-    const boxes = [useRef(null), useRef(null), useRef(null), useRef(null)];
+// ----------------------------------------------------------------------
+
+export default function OTPAuthenticationForm() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const email = location.state.email;
     const [values, setValues] = useState(['', '', '', '']);
-    const handleChange = (event, index) => {
-        const newValue = event.target.value;
-        if (/^\d*$/.test(newValue) && newValue.length <= 1) {
-            const newValues = [...values];
-            newValues[index] = newValue;
-            setValues(newValues);
-            if (index < 3 && newValue.length) {
-                boxes[index + 1].current.focus();
-            }
+    const boxes = [useRef(null), useRef(null), useRef(null), useRef(null)];
+
+    const handleChange = (e, index) => {
+        const newValues = [...values];
+        newValues[index] = e.target.value;
+        setValues(newValues);
+
+        if (e.target.value === '' && index !== 0) {
+            boxes[index - 1].current.focus();
+        } else if (index !== 3) {
+            boxes[index + 1].current.focus();
         }
     };
 
-    const handleSubmit = () => {
-        const otp = values.join('');
-        console.log('OTP:', otp);
+    const handleClick = async () => {
+        try {
+            const otp = values.join('');
+            const data = { otp, email };
+            const response = await fetch('https://hqtbe.site/api/v1/users/register/otp/verify', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            navigate('/login');
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
         <Stack sx={{ mt: 10, mx: 'auto', maxWidth: '480px' }} spacing={3}>
-
             <Stack direction="row" spacing={2}>
                 <TextField
                     inputRef={boxes[0]}
@@ -59,7 +83,7 @@ export default function OTP() {
                 />
             </Stack>
 
-            <Button variant="contained" size="large" onClick={handleSubmit}>
+            <Button variant="contained" size="large" onClick={handleClick}>
                 Xác thực
             </Button>
         </Stack>
