b9aa20b6de627b09fa97144934e4224bbf07e1fe
Thái Thành Phát <skphat789@gmail.com>
2023-03-08 11:38:56 +0700

    Login Author ADMIN-USER

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Navigate, useLocation} from "react-router-dom"
+import { useSelector } from 'react-redux';
 
 const ProtectedRoute = ({children,loginInfo}) => {
+    const currentUser = useSelector((state) => state.user.current);
     const location = useLocation();
-    if((loginInfo == null && !localStorage.getItem('access-token') )) {
+    if((loginInfo == null && !localStorage.getItem('access-token'))) {
         console.log("Is Authen",loginInfo);
         return <Navigate to="/login" state={{ from: location}} replace />
     }
