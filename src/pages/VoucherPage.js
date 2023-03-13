0ff039e08c20fe110d1e64333f11530737042cc8
vungmai-fptu <vungocmai322@gmail.com>
2023-03-14 06:43:08 +0700

    update Voucher Page/List/Card

--- src/pages/VoucherPage.js
+++ src/pages/VoucherPage.js
@@ -1,22 +1,35 @@
-import { Helmet } from 'react-helmet-async';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-// @mui
 import { Container, Stack, Typography } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
-// components
-import { VoucherSort, VoucherList, VoucherCartWidget, VoucherFilterSidebar } from '../sections/@dashboard/voucher';
+import { Helmet } from 'react-helmet-async';
+import axios from 'axios';
 import Iconify from '../components/iconify';
-// mock
-import VOUCHERS from '../_mock/vouchers';
-
-// ----------------------------------------------------------------------
+import { VoucherSort, VoucherList, VoucherCartWidget, VoucherFilterSidebar } from '../sections/@dashboard/voucher';
 
 export default function VoucherPage() {
   const navigate = useNavigate();
   const [openFilter, setOpenFilter] = useState(false);
   const currentUser = useSelector((state) => state.user.current);
+  const [vouchers, setVouchers] = useState([]);
+
+  useEffect(() => {
+    async function fetchVouchers() {
+      try {
+        const response = await axios.get('https://hqtbe.site/api/v1/vouchers/getAllVoucher?page=1&size=10&sortBy=id&sortType=asc', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('access-token')}`,
+          },
+        });
+        setVouchers(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchVouchers();
+  }, []);
 
   const handleOpenFilter = () => {
     setOpenFilter(true);
@@ -31,18 +44,13 @@ export default function VoucherPage() {
       <Helmet>
         <title> Cóc Phượt </title>
       </Helmet>
-
       <Container>
         <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
           <Typography variant="h4" gutterBottom>
             Tất cả phiếu giảm giá hiện có
           </Typography>
           {currentUser.role === 'ADMIN' ? (
-            <LoadingButton
-              onClick={() => navigate('/voucher')}
-              variant="contained"
-              startIcon={<Iconify icon="eva:plus-fill" />}
-            >
+            <LoadingButton onClick={() => navigate('/voucher')} variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
               Tạo ưu đãi mới
             </LoadingButton>
           ) : (
@@ -51,17 +59,11 @@ export default function VoucherPage() {
         </Stack>
         <Stack direction="row" flexWrap="wrap-reverse" alignItems="center" justifyContent="flex-end" sx={{ mb: 5 }}>
           <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
-            <VoucherFilterSidebar
-              openFilter={openFilter}
-              onOpenFilter={handleOpenFilter}
-              onCloseFilter={handleCloseFilter}
-            />
+            <VoucherFilterSidebar openFilter={openFilter} onOpenFilter={handleOpenFilter} onCloseFilter={handleCloseFilter} />
             <VoucherSort />
           </Stack>
         </Stack>
-
-        <VoucherList vouchers={VOUCHERS} />
-        
+        <VoucherList vouchers={vouchers} />
       </Container>
     </>
   );
--- src/sections/@dashboard/voucher/VoucherList.js
+++ src/sections/@dashboard/voucher/VoucherList.js
@@ -1,9 +1,7 @@
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-// @mui
 import { Grid } from '@mui/material';
-import ShopVoucherCard from './VoucherCard';
-
-// ----------------------------------------------------------------------
+import VoucherCard from './VoucherCard';
 
 VoucherList.propTypes = {
   vouchers: PropTypes.array.isRequired,
@@ -14,7 +12,7 @@ export default function VoucherList({ vouchers, ...other }) {
     <Grid container spacing={3} {...other}>
       {vouchers.map((voucher) => (
         <Grid key={voucher.id} item xs={12} sm={6} md={3}>
-          <ShopVoucherCard voucher={voucher} />
+          <VoucherCard voucher={voucher} />
         </Grid>
       ))}
     </Grid>
