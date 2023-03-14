29393802a35440c3e5bc038852a18df0182542f4
Thái Thành Phát <skphat789@gmail.com>
2023-03-14 15:07:18 +0700

    admin dashboard

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -5,6 +5,10 @@ const adminApi = {
         const url = '/users';
         return axiosClient.get(url);
     },
+    getListVoucher: () => {
+        const url = '/vouchers';
+        return axiosClient.get(`${url}?page=1&size=200`);
+    },
 }
 
 export default adminApi;
